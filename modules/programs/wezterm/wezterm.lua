665ad51481c109355b947f8d4192e6b3f97350bb
Sindre Moen <sindrenm@gmail.com>
2025-08-12 02:44:08 +0200

    Add fallback fonts to Wezterm

--- modules/programs/wezterm/wezterm.lua
+++ modules/programs/wezterm/wezterm.lua
@@ -1,6 +1,12 @@
 return {
-  color_scheme = 'JetBrains Darcula',
-  font = wezterm.font('JetBrains Mono'),
+  color_scheme = "JetBrains Darcula",
+  font = wezterm.font_with_fallback {
+    "JetBrains Mono",
+    "Font Awesome 6 Free Solid",
+    "Font Awesome 6 Free Regular",
+    "Font Awesome 6 Brands Regular",
+    "Noto Color Emoji",
+  },
 
   enable_tab_bar = false,
 
@@ -10,14 +16,14 @@ return {
   window_decorations = "NONE",
 
   window_frame = {
-    border_left_width = '2px',
-    border_right_width = '2px',
-    border_bottom_height = '2px',
-    border_top_height = '2px',
-    border_left_color = 'white',
-    border_right_color = 'white',
-    border_bottom_color = 'white',
-    border_top_color = 'white',
+    border_left_width = "2px",
+    border_right_width = "2px",
+    border_bottom_height = "2px",
+    border_top_height = "2px",
+    border_left_color = "white",
+    border_right_color = "white",
+    border_bottom_color = "white",
+    border_top_color = "white",
   },
 
   window_padding = {
