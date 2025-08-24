f724813815bd8bc1aed9367c6c89f91a90857c35
Sindre Moen <sindrenm@gmail.com>
2025-08-24 15:04:48 +0200

    Configure tmux

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -1,5 +1,28 @@
+{ pkgs, ... }:
+
+let
+  conf = builtins.readFile ./tmux/tmux.conf;
+in
 {
   programs.tmux = {
     enable = true;
+
+    keyMode = "vi";
+    mouse = true;
+    clock24 = true;
+    baseIndex = 1;
+    shortcut = "a";
+    escapeTime = 50;
+    historyLimit = 1000000;
+
+    plugins = with pkgs.tmuxPlugins; [
+      continuum
+      resurrect
+      sensible
+      tmux-thumbs
+      yank
+    ];
+
+    extraConfig = conf;
   };
 }
