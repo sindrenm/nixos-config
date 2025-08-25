fc83ef956376f408f66d858ad53095ee3e8322b5
Sindre Moen <sindrenm@gmail.com>
2025-08-25 07:55:47 +0200

    Change theme to Catppuccin Frappé

--- modules/themes/current.nix
+++ modules/themes/current.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    ./color-schemes/gruvbox-dark-hard.nix
+    ./color-schemes/catppuccin-frappe.nix
     ./cursors/bibata-modern-ice.nix
     ./fonts/default.nix
     ./wallpapers/zelda-triforce.nix
