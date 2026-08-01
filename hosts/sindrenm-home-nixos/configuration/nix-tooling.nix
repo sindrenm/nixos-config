88669bf0a720164b8165feaa321067172278bebe
Sindre Moen <sindrenm@gmail.com>
2026-08-01 17:04:21 +0200

    feat(nix-tooling): link libsecret hrough nix-ld

--- hosts/sindrenm-home-nixos/configuration/nix-tooling.nix
+++ hosts/sindrenm-home-nixos/configuration/nix-tooling.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.nh = {
@@ -9,5 +9,10 @@
     clean.extraArgs = "--keep-since 4d --keep 3";
   };
 
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      libsecret
+    ];
+  };
 }
