2b6c082edc78134b5541ae6f17b60757ab22584a
Sindre Moen <sindrenm@gmail.com>
2026-08-01 16:28:35 +0200

    fix(hyprland): add missing UWSM setup

--- hosts/sindrenm-home-nixos/configuration/desktop/hyprland.nix
+++ hosts/sindrenm-home-nixos/configuration/desktop/hyprland.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 
 {
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
 
   xdg.portal.extraPortals = [
     pkgs.xdg-desktop-portal-hyprland
