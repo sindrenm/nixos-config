451cac150dc3ac7b76fa2e8f8fc084a3c2e2b897
Sindre Moen <sindrenm@gmail.com>
2026-08-06 01:12:29 +0200

    feat(shell-prompt): configure Starship

---
+++ home/shell-prompt.nix
@@ -0,0 +1,25 @@
+{ ... }:
+
+{
+  programs.starship = {
+    enable = true;
+
+    settings = {
+      git_branch.disabled = true;
+      git_commit.disabled = true;
+
+      custom.jj = {
+        command = "prompt";
+        format = "$output";
+        ignore_timeout = true;
+        shell = [
+          "starship-jj"
+          "--ignore-working-copy"
+          "starship"
+        ];
+        use_stdin = false;
+        when = true;
+      };
+    };
+  };
+}
