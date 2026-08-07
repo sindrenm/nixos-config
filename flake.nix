5826c4eca1dcc8b4e20faff83020731b0f30ed72
Sindre Moen <sindrenm@gmail.com>
2026-08-07 04:08:42 +0200

    chore(nix): update flake

--- flake.nix
+++ flake.nix
@@ -56,8 +56,6 @@
               nixpkgs.overlays = [
                 claude-desktop.overlays.default
                 neovim-nightly-overlay.overlays.default
-
-                (import ./overlays/hyprland-glaze-patch.nix)
               ];
             }
 
