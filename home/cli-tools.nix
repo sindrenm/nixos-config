1991a47d3ed81e6327d50aa318bb3c95c7feae9f
Sindre Moen <sindrenm@gmail.com>
2026-08-08 21:18:02 +0200

    feat(cli-tools): configure fd through Home Manager

--- home/cli-tools.nix
+++ home/cli-tools.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }:
 
 {
+  programs.fd = {
+    enable = true;
+    hidden = true;
+    ignores = [ ".git/" ".jj/" ];
+  };
+
   home.packages = with pkgs; [
     atuin
     bat
     carapace
-    fd
     ripgrep
     speedtest-cli
     starship
