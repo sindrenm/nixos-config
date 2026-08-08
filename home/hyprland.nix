a762eb77d5957a236ee8c7f95a307a8a7d58d4d2
Sindre Moen <sindrenm@gmail.com>
2026-08-09 00:56:51 +0200

    feat(hyprland): configure hypridle

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -2,8 +2,24 @@
 
 {
   home.packages = with pkgs; [
-    hypridle
     hyprlock
     hyprpolkitagent
   ];
+
+  services.hypridle = {
+    enable = true;
+
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+
+      listener = {
+        timeout = 300; # 5 min
+        on-timeout = "loginctl lock-session";
+      };
+    };
+  };
 }
