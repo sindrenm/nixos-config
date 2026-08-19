e9f3bda24d7caf66905a8a801616d5d0ca7eb496
Sindre Moen <sindrenm@gmail.com>
2026-08-19 11:15:39 +0200

    fix(android): add missing environment variable
    Add ANDROID_HOME environment variable as a session variable, a systemd
    user variable and also as a Nushell variable, since these don't seem to
    talk to each other.

--- modules/development/android.nix
+++ modules/development/android.nix
@@ -1,10 +1,21 @@
 {
-  home-manager.users.sindre = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      android-cli
-      android-tools
-      androidStudioPackages.canary
-      androidStudioPackages.stable
-    ];
-  };
+  home-manager.users.sindre =
+    { pkgs, config, ... }:
+    let
+      androidEnvVariables = {
+        ANDROID_HOME = "${config.xdg.dataHome}/android-sdk";
+      };
+    in
+    {
+      home.packages = with pkgs; [
+        android-cli
+        android-tools
+        androidStudioPackages.canary
+        androidStudioPackages.stable
+      ];
+
+      home.sessionVariables = androidEnvVariables;
+      programs.nushell.environmentVariables = androidEnvVariables;
+      systemd.user.sessionVariables = androidEnvVariables;
+    };
 }
