98bfe1d97f816db9b607081e3f47d5c64a0fe1cf
Sindre Moen <sindrenm@gmail.com>
2026-09-04 10:38:01 +0200

    fix(editors): move keys out of `messagesopt`
    https://github.com/neovim/neovim/pull/41474

--- modules/editors/nvim/lua/ui.lua
+++ modules/editors/nvim/lua/ui.lua
@@ -13,12 +13,14 @@ vim.cmd.colorscheme("catppuccin")
 
 icons.setup()
 
+-- ui2 reads the message timeout and the cmdline max height from 'messagesopt'.
+vim.o.messagesopt = vim.o.messagesopt .. ",maxheight:50,timeout:5000"
+
 ui2.enable({
   enable = true,
   msg = {
     targets = "msg", -- or "msg" for floating window
-    cmd = { height = 0.5 },
-    msg = { height = 0.5, timeout = 5000 },
+    msg = { height = 0.5 },
     pager = { height = 0.5 },
   },
 })
