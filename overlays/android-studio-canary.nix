5f0be617e9994fa55e9cf4210c338793174f5d1e
Sindre Moen <sindrenm@gmail.com>
2026-09-09 08:24:49 +0200

    chore(android-studio): update canary

--- overlays/android-studio-canary.nix
+++ overlays/android-studio-canary.nix
@@ -6,11 +6,11 @@ final: prev:
 # When updating, fetch the latest entry from https://jb.gg/android-studio-releases-list.json, and
 # update version/url/sha256Hash below.
 let
-  version = "2026.2.1.3"; # "Android Studio Rabbit 1 | 2026.2.1 Canary 3"
+  version = "2026.2.1.4"; # "Android Studio Rabbit 1 | 2026.2.1 Canary 4"
   sources = {
     x86_64-linux = {
-      url = "https://edgedl.me.gvt1.com/android/studio/ide-zips/${version}/android-studio-rabbit1-canary3-linux.tar.gz";
-      sha256Hash = "2f8e1796d8386ea2f3dd5a6e9f8ad306787d1cf8a75a6d2bb32e1df14d593d01";
+      url = "https://edgedl.me.gvt1.com/android/studio/ide-zips/${version}/android-studio-rabbit1-canary4-linux.tar.gz";
+      sha256Hash = "141e6db01119045d17a73b4974bbf420e4e6c7f46e209fe8ee8781ddb822cf3b";
     };
   };
   inherit (prev.androidStudioPackages.canary) pname meta;
